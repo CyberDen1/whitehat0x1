250a5aad635cd1370d9c850a99640c908d208705
CyberDen <110781601+CyberDen1@users.noreply.github.com>
2022-11-25 18:30:15 +0300

    Add files via upload

---
+++ strcpy.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+char buffer[64];
+if (argc < 2)
+{
+printf("ERROR - Vy dolzhny predostavit kak minimum odin argument\n");
+return 1;
+}
+strcpy(buffer, argv[1] );
+return 0;
+}
